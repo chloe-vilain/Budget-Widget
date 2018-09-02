2ff8e693d6408e91a7da0190689d8640d1e959fe
cwvilain <cwvilain@gmail.com>
2018-09-02 15:39:29 -0400

    Add car cost slider.
    Add car slider in the same style as all other sliders in the app. Add
    create_carScale function, update_car_cost function in GUI.py. Add
    set_car_mon in Budget.py. Updated Budget init to include car costs.

--- Budget.py
+++ Budget.py
@@ -4,11 +4,12 @@ class Budget(object):
 	can be set from the GUI class
 	"""
 
-	def __init__(self, savings, lodging_mon, gas_mon, fixed_costs_mon, incidental_mon):
+	def __init__(self, savings, lodging_mon, car_mon, gas_mon, fixed_costs_mon, incidental_mon):
 		""" Initialize the budget program
 		"""
 		self.savings = savings
 		self.lodging_mon = lodging_mon
+		self.car_mon = car_mon
 		self.gas_mon = gas_mon
 		self.fixed_costs_mon = fixed_costs_mon
 		self.incidental_mon = incidental_mon
@@ -16,22 +17,23 @@ class Budget(object):
 	def get_burndown_rate(self):
 		""" Return the monthly burn-down rate based on all known costs
 		"""
-		return self.lodging_mon + self.gas_mon + self.fixed_costs_mon + self.incidental_mon
+		return self.lodging_mon + self.car_mon + self.gas_mon + self.fixed_costs_mon + self.incidental_mon
 
 	def get_months_remaining(self):
 		return self.savings/ self.get_burndown_rate()
 
 	def set_lodging_mon(self, new):
 		self.lodging_mon = int(new)
-		#print self.lodging_mon
 
 	def set_incidental_mon(self, new):
 		self.incidental_mon = int(new)
-		#print type (self.incidental_mon)
-		#print self.get_months_remaining()
 
-my_budget = Budget(27000, 400, 100, 400, 400)
+	def set_car_mon(self, new):
+		self.car_mon = int(new)
+"""
+my_budget = Budget(27000, 400, 400, 100, 400, 400)
 print my_budget.get_burndown_rate()
-print my_budget.get_months_remaining()  
+print my_budget.get_months_remaining() 
+""" 
 
 
--- GUI.py
+++ GUI.py
@@ -11,16 +11,25 @@ class GUI(tk.Frame):
 
 	def createWidgets(self):
 		self.create_lodgingScale()
+		self.create_carScale()
 		self.create_incidentalScale()	
 		self.create_budgetStatus()
 		self.create_quitButton()
 
 	def create_lodgingScale(self, from_ = 0, to = 1000, length = 300):
 		self.lodgingScale = tk.Scale(self, orient=tk.HORIZONTAL, from_ = from_, to = to, length = length, label = 'Lodging Cost', command = self.update_lodging_cost)
+		self.lodgingScale.set(self.budget.lodging_mon)
 		self.lodgingScale.grid()
+
+	def create_carScale(self, from_ = 0, to = 700, length = 300):
+		self.carScale = tk.Scale(self, orient=tk.HORIZONTAL, from_ = from_, to = to, length = length, label = 'Car Cost', command = self.update_car_cost)
+		self.carScale.set(self.budget.car_mon)
+		self.carScale.grid()
+
 
 	def create_incidentalScale(self, from_ = 0, to = 1000, length = 300):
 		self.incidentalScale = tk.Scale(self, orient=tk.HORIZONTAL, from_ = from_, to = to, length = length, label = 'Incidental Cost', command = self.update_incidental_cost)
+		self.incidentalScale.set(self.budget.incidental_mon)
 		self.incidentalScale.grid()
 
 	def create_budgetStatus(self):
@@ -36,6 +45,10 @@ class GUI(tk.Frame):
 		self.budget.set_lodging_mon(new)
 		self.update_budgetStatus()
 
+	def update_car_cost(self, new):
+		self.budget.set_car_mon(new)
+		self.update_budgetStatus()
+
 	def update_incidental_cost(self, new):
 		self.budget.set_incidental_mon(new)
 		self.update_budgetStatus()
@@ -45,7 +58,7 @@ class GUI(tk.Frame):
 		b = self.budget.get_burndown_rate()
 		self.budgetStatus.configure(text = "Monthly burndown: %r Total months: %r" %(b, m))
 
-my_budget = Budget(27000, 400, 100, 400, 400)
+my_budget = Budget(27000, 400, 400, 100, 400, 400)
 app = GUI(my_budget)
 app.master.title('Budget Application')
 app.mainloop()
